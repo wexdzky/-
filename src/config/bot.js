9eadadde92dc673d2d5aa4f325fd73d38bba068c
wexdzky <issiakanahi85@gmail.com>
2026-05-17 16:40:55 +0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: ".gg/?mk",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#9E1800", 
+      secondary: "#FFFFFF", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
